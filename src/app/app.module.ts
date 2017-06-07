73f835908e1d2432d031ebd89f2dd26f7d118635
Adrian Henry <adrian@headspin.io>
2017-06-07 23:01:08 +0000

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+
+import '../styles/styles.scss';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+  ],
+  declarations: [
+    AppComponent
+  ],
+  providers: [ ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
